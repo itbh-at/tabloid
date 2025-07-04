9e98af6c870b2cf7a8f35c520a437ebfb6876e22
thomaskim09 <thomaskim.info@gmail.com>
2025-07-04 08:55:34 +0800

    Refine: Rename artifactId, add timezone field

--- src/main/java/at/itbh/tabloid/model/Document.java
+++ src/main/java/at/itbh/tabloid/model/Document.java
@@ -5,10 +5,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record Document(
-        @JsonProperty("title") String title,
-        @JsonProperty("author") String author,
-        @JsonProperty("subject") String subject,
-        @JsonProperty("keywords") List<String> keywords,
-        @JsonProperty("locale") String locale,
-        @JsonProperty("formats") List<FormatOptions> formats) {
+                @JsonProperty("title") String title,
+                @JsonProperty("author") String author,
+                @JsonProperty("subject") String subject,
+                @JsonProperty("keywords") List<String> keywords,
+                @JsonProperty("locale") String locale,
+                @JsonProperty("timezone") String timezone,
+                @JsonProperty("formats") List<FormatOptions> formats) {
 }
